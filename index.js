ef3dd7e336aea9d4c984c8515efb5d64f51fabcd
Olly <ollydutton@hotmail.com>
2016-04-30 16:26:59 +1000

    added date func

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ var path = require("path");
 var fs = require("fs");
 var readlineSync = require('readline-sync');
 var _ = require("lodash");
+var moment = require("moment");
 var data = require("./data/data-maka.json");
 var options = require("./options");
 var names = require("./data/names");
 var emails = require("./data/emails");
-var dataSize = 1;
-var dataArr = [];
+var dataSize = 1,
+    dataArr = [],
+    days = 30;
 
 //Load all names once
 var firstNames = names.firstNames(); 
 var lastNames = names.lastNames(); 
 
 var optsArr = options.argv._;
+var keyVals = options.argv;
 
 // If the user didn't specify the number of objects, prompt for that info...
 if (!options.argv.objs) {
-    var ok = readlineSync.keyInYN('You didn\'t specify your data size. The default is 1 JSON object. Is this ok? ');
+    var ok = readlineSync.keyInYN('You didn\'t specify how many JSON objects you require. The default is 1 JSON object. Is this ok? ');
     if (!ok) { 
         objs = readlineSync.question('How many JSON objects would you like? ');
-        // Check for valid number
         checkNaN(objs);
     }
-} else { // delete objs key from options.argv
+} else { 
     dataSize = options.argv.objs;
-    delete options.argv.objs;
 }
 
 function checkNaN(num) {
@@ -38,12 +39,38 @@ function checkNaN(num) {
     }
 }
 
-_.times(dataSize, function() {
-    checkArgs();
+
+if (_.has(keyVals, 'date')) { 
+    addDates();
+}
+
+function addDates() {
+    var obj = {};
+    days = _.get(keyVals, 'date')
+
+    var endDate = moment();
+    var startDate = moment().subtract(30, "days"); 
+
+    var dates = [];
+
+    var currDate = startDate.clone().startOf('day');
+    var lastDate = endDate.clone().startOf('day');
+
+    while(currDate.add('days', 1).diff(lastDate) < 0) {
+        dates.push(currDate.clone().format("DD-MM-YYYY")); // add user formatting
+    }
+    obj.date = dates;
+    echo(obj);
+}
+
+_.times(dataSize, function(i) {
+    var dataObj = {};
+    dataObj.id = i+1;
+    checkArgsArr(dataObj);
 });
 
-function checkArgs() {
-    var dataObj = {};
+
+function checkArgsArr(dataObj) {
     if (optsArr.indexOf('name') > -1 && optsArr.indexOf('email') < 0) { 
         getNames(dataObj); 
     } 
@@ -52,6 +79,9 @@ function checkArgs() {
     }
     if (optsArr.indexOf('name') > -1 && optsArr.indexOf('email') > -1) { 
         getNames(dataObj, true); 
+    }
+    if (optsArr.indexOf('date')) {
+
     }
 };
 
@@ -69,6 +99,9 @@ function getEmails(dataObj, name) {
     } else {
         dataObj.email = _.sample(firstNames).toLowerCase() + _.sample(emails.emailStems());
     }
+echo(dataObj);
+dataArr.push(dataObj);
+echo(dataArr);
 } 
 
 function emailsAndNames(dataObj) {
